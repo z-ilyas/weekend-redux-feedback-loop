00116846688fc1e5dd3adeef2ae1ed82d1c9fafa
Ilyas Mohamed <zswaleh@ymail.com>
2023-05-05 15:42:15 -0500

    added basic redux boiler plate

--- src/components/Feedback/Feelings.jsx
+++ src/components/Feedback/Feelings.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+// import { useSelector, useDispatch } from 'react-redux';
+
 function Feelings() {
 
+    const [ feelings, setfeelings] = useState('')
+    // const feelingsObj = useSelector( (store) => store.feelings );
+
+    // const dispatch = useDispatch();
+    
     return (
         <div className="feelings">
             <h1>How are you Feeling today?</h1>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const feedBack = {}
+const feelings = (state = feedBack, action) => {
+    return state;
+}
+
+const theStore = createStore(
+    combineReducers({
+        feelings
+    }),
+    applyMiddleware(
+      logger
+    )
+  )
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store = {theStore}>
             <App />
+        </Provider>
     </React.StrictMode>
 );
