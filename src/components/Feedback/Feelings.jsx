dd195c423deb298e1573db96c46ace35108ece90
Ilyas Mohamed <zswaleh@ymail.com>
2023-05-05 15:27:23 -0500

    My feelings components shows all the necessary stuff on the DOM

--- src/components/Feedback/Feelings.jsx
+++ src/components/Feedback/Feelings.jsx
@@ -1,5 +1,17 @@
 function Feelings() {
-    
+
+    return (
+        <div className="feelings">
+            <h1>How are you Feeling today?</h1>
+            <p>Feeling?</p>
+            <input
+                type="number"
+                value={feelings}
+                onChange={ (event) => {setfeelings(event.target.value)}}
+            />
+            <button>NEXT</button>
+        </div>
+    )
 }
 
 export default Feelings;
