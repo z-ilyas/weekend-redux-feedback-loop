63e8a9904e7c0494ccde4713b004958050c1bc8a
Ilyas Mohamed <zswaleh@ymail.com>
2023-05-05 16:58:00 -0500

    the alert button works when no input has been put

--- src/components/Feedback/Feelings.jsx
+++ src/components/Feedback/Feelings.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Feelings() {
-
-    const [ feelings, setfeelings] = useState('');
+    
+    // const feelingsObj = useSelector(state => state.feelings);
+    const [feelings, setfeelings] = useState('');
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const newFeelings = () => {
-        dispatch({
-            type: 'ADD_FEELINGS',
-            payload: feelings
-        })
-        history.push('/Understanding');
+    const noInput = () => {
+        if( feelings === ''){
+            Swal.fire({
+                icon: 'error',
+                title: 'PLEASE PUT INPUT',
+              })
+        }else{
+            dispatch({
+                type: 'ADD_FEELINGS',
+                payload: feelings
+            })
+            history.push('/Understanding');
+            noInput();
+        }
     }
+ 
     
     return (
         <div className="feelings">
@@ -25,7 +36,7 @@ function Feelings() {
                 value={feelings}
                 onChange={ (event) => {setfeelings(event.target.value)}}
             />
-            <button onClick={newFeelings}>NEXT</button>
+            <button onClick={noInput}>NEXT</button>
         </div>
     )
 }
